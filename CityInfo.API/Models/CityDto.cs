22f85949caa3ccbe3089c8416044a44e65b1dba3
brunocs90@gmail.com <brunocs90@gmail.com>
2023-03-11 13:49:52 -0300

    return correct status code

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -8,6 +8,14 @@
 
         public string? Description { get; set; }
 
-        //public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
     }
 }
