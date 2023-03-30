514f206d6cf6bb57f0d0fe771c0296c90d16c993
brunocs90@gmail.com <brunocs90@gmail.com>
2023-03-30 09:05:32 -0300

    securing your api

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -15,5 +15,7 @@ namespace CityInfo.API.Services
         Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInterest);
         void DeletePointOfInterest(PointOfInterest pointOfInterest);
         Task<bool> SaveChangesAsync();
+
+        Task<bool> CityNameMatchesCityId(string? cityName, int cityId);
     }
 }
